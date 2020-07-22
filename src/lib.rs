cc0735c25e80f5ad8e296d1246b3743ebfbc16a8
Keane O'Kelley <me@keane.space>
2020-07-21 21:23:32 -0400

    Negligible changes

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,8 @@ use lazy_static::lazy_static;
 use std::net::Shutdown::Both;
 use std::collections::HashMap;
 use std::error::Error;
-pub use crate::util::{load_static_file, gen_http_error, walk_params, split_string, get_client_addr, log};
+pub use crate::util::{fmt_http_error, load_static_file, gen_http_error, walk_params, split_string, get_client_addr, log};
 use boolinator::Boolinator;
-use crate::util::{fmt_http_error};
 
 mod util;
 
@@ -98,7 +97,7 @@ pub fn router(request: &HTTPRequest, routes: Vec<(Regex, Handler)>) -> HTTPRespo
 
 
 impl HTTPRequest {
-    fn new(method: String, path: String, proto: String) -> HTTPRequest {
+    pub fn new(method: String, path: String, proto: String) -> HTTPRequest {
         HTTPRequest {
             method,
             path,
@@ -135,7 +134,7 @@ lazy_static! {
     static ref HTTP_VERSION_REGEX: Regex = Regex::new(r"^HTTP/[\d]\.[\d]$").unwrap();
 }
 
-fn router_404(_request: &HTTPRequest, mut response: HTTPResponse) -> HTTPResult {
+pub fn router_404(_request: &HTTPRequest, mut response: HTTPResponse) -> HTTPResult {
     gen_http_error(HTTP_404, &mut response);
     Ok(response)
 }
